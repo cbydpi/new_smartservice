fa9d7f35615fef0f2e7cbdafb150f5df0476936f
cbydpi <960576010@qq.com>
2018-07-13 18:34:15 +0800

    场景移植

--- src/views/sence/index.vue
+++ src/views/sence/index.vue
@@ -2,14 +2,14 @@
   <div>
     <!--这是场景录入页面,-->
 
-    <rich-text @richTextContent='getRichTextData'></rich-text>
+    <!--<rich-text @richTextContent='getRichTextData'></rich-text>-->
     <sence-modul ref='diag' v-bind:model-data='diagramData' v-on:model-changed='modelChanged' v-on:changed-selection='changedSelection'></sence-modul>
-    <button v-on:click='addNode'>Add Child to Gamma</button>
+    <!--<button v-on:click='addNode'>Add Child to Gamma</button>
     <button v-on:click='modifyStuff'>Modify view model data without undo</button>
     <br/>Current Node:
     <input v-model.lazy='currentNodeText' v-bind:disabled='currentNode === null' />
     <br/>The saved GoJS Model:
-    <textarea style='width:100%;height:250px'>{{ savedModelText }}</textarea>
+    <textarea style='width:100%;height:250px'>{{ savedModelText }}</textarea>-->
   </div>
 </template>
 
@@ -37,80 +37,148 @@
       return {
         diagramData: { // passed to <diagram> as its modelData
           nodeDataArray: [{
-            key: 1,
-            text: 'Alpha',
-            color: 'lightblue'
-          },
-          {
-            key: 2,
-            text: 'Beta',
-            color: 'orange'
-          },
-          {
-            key: 3,
-            text: 'Gamma',
-            color: 'lightgreen'
-          },
-          {
-            key: 4,
-            text: 'Delta',
-            color: 'pink'
-          }
-          ],
-          linkDataArray: [{
-            from: 1,
-            to: 2
-          },
-          {
-            from: 1,
-            to: 3
-          },
-          {
-            from: 3,
-            to: 4
-          }
-          ]
-        },
-        currentNode: null,
-        savedModelText: '',
-        counter: 1, // used by addNode
-        counter2: 4 // used by modifyStuff
+            'id': 1636,
+            'contentId': null,
+            'senceNo': '43',
+            'content': '奥法的产品优势有：1.从通用化到场景化。2.从标准化到个性化。3.智能巡检。4.从技术到数据应用。您想详细了解哪个呢？',
+            'level': 0,
+            'parentId': 0,
+            'keyword': '(奥法).*(产品).*(优势)',
+            'description': '奥法产品的优势是什么',
+            'status': 0,
+            'weight': null,
+            'username': 'admin',
+            'contentSign': 1610,
+            'voiceId': null,
+            'voice': null,
+            'remark': null,
+            'extendList': null,
+            'commadList': [],
+            'contentType': 1,
+            'cbContent': ''
+          }, {
+            'id': 1637,
+            'contentId': null,
+            'senceNo': '43',
+            'content': '通用化到场景化，既可以实现通用法问答服务，也拥有灵活的场景化服务，引导用户与机器人流畅的对话，多轮交互，提高体验。',
+            'level': 1,
+            'parentId': 1636,
+            'keyword': '(通用化)',
+            'description': '通用化',
+            'status': 2,
+            'weight': null,
+            'username': 'admin',
+            'contentSign': 1612,
+            'voiceId': null,
+            'voice': null,
+            'remark': null,
+            'extendList': null,
+            'commadList': [],
+            'contentType': 1,
+            'cbContent': ''
+          }, {
+            'id': 1638,
+            'contentId': null,
+            'senceNo': '43',
+            'content': '从标准化到个性化，是指，针对不同的业务领域，将配置不同的数据训练模式、积累不同自学习逻辑，达到精准服务。',
+            'level': 1,
+            'parentId': 1636,
+            'keyword': '(标准化)',
+            'description': '标准化',
+            'status': 2,
+            'weight': null,
+            'username': 'admin',
+            'contentSign': 1613,
+            'voiceId': null,
+            'voice': null,
+            'remark': null,
+            'extendList': null,
+            'commadList': [],
+            'contentType': 1,
+            'cbContent': ''
+          }, {
+            'id': 1639,
+            'contentId': null,
+            'senceNo': '43',
+            'content': '智能巡检是指，实时监控数据，准确把握数据积累过程，确保知识的准确性。',
+            'level': 1,
+            'parentId': 1636,
+            'keyword': '(智能).*(巡检)',
+            'description': '智能巡检',
+            'status': 2,
+            'weight': null,
+            'username': 'admin',
+            'contentSign': 1614,
+            'voiceId': null,
+            'voice': null,
+            'remark': null,
+            'extendList': null,
+            'commadList': [],
+            'contentType': 1,
+            'cbContent': ''
+          }, {
+            'id': 1640,
+            'contentId': null,
+            'senceNo': '43',
+            'content': '从技术到数据应用，我们不仅会产生一个专业的知识图谱，以及基于专用图谱之上的自然语言理解的技术，提升整个领域的生产力和行业效率。在后期数据清洗与算法优化的基础上，实现可视化的舆情数据报告和对趋势的智能化预测。',
+            'level': 1,
+            'parentId': 1636,
+            'keyword': '(数据)',
+            'description': '数据',
+            'status': 2,
+            'weight': null,
+            'username': 'admin',
+            'contentSign': 1615,
+            'voiceId': null,
+            'voice': null,
+            'remark': null,
+            'extendList': null,
+            'commadList': [],
+            'contentType': 1,
+            'cbContent': ''
+          }],
+          'class': 'go.TreeModel'
+        }
+//      currentNode: null,
+//      savedModelText: '',
+//      counter: 1, // used by addNode
+//      counter2: 4 // used by modifyStuff
       }
     },
     computed: {
-      currentNodeText: {
-        get: function () {
-          var node = this.currentNode
-          if (node instanceof go.Node) {
-            return node.data.text
-          } else {
-            return ''
-          }
-        },
-        set: function (val) {
-          var node = this.currentNode
-          if (node instanceof go.Node) {
-            var model = this.model()
-            model.startTransaction()
-            model.setDataProperty(node.data, 'text', val)
-            model.commitTransaction('edited text')
-          }
-        }
-      }
+//    currentNodeText: {
+//      get: function () {
+//        var node = this.currentNode
+//        if (node instanceof go.Node) {
+//          return node.data.text
+//        } else {
+//          return ''
+//        }
+//      },
+//      set: function (val) {
+//        var node = this.currentNode
+//        if (node instanceof go.Node) {
+//          var model = this.model()
+//          model.startTransaction()
+//          model.setDataProperty(node.data, 'text', val)
+//          model.commitTransaction('edited text')
+//        }
+//      }
+//    }
     },
     methods: {
-      getRichTextData (data) {
-        console.log(data)
-      },
+//    getRichTextData (data) {
+//      console.log(data)
+//    },
       // get access to the GoJS Model of the GoJS Diagram
-      model: function () {
-        return this.$refs.diag.model()
-      },
+//    model: function () {
+//      return this.$refs.diag.model()
+//    },
 
       // tell the GoJS Diagram to update based on the arbitrarily modified model data
-      updateDiagramFromData: function () {
-        this.$refs.diag.updateDiagramFromData()
-      },
+//    updateDiagramFromData: function () {
+//      this.$refs.diag.updateDiagramFromData()
+//    },
 
       // this event listener is declared on the <diagram>
       modelChanged: function (e) {
@@ -129,51 +197,51 @@
           this.currentNode = null
           this.currentNodeText = ''
         }
-      },
+      }
 
       // Here we modify the GoJS Diagram's Model using its methods,
       // which can be much more efficient than modifying some memory and asking
       // the GoJS Diagram to find differences and update accordingly.
       // Undo and Redo will work as expected.
-      addNode: function () {
-        var model = this.model()
-        model.startTransaction()
-        model.setDataProperty(model.findNodeDataForKey(4), 'color', 'purple')
-        var data = {
-          text: 'NEW ' + this.counter++,
-          color: 'yellow'
-        }
-        model.addNodeData(data)
-        model.addLinkData({
-          from: 3,
-          to: model.getKeyForNodeData(data)
-        })
-        model.commitTransaction('added Node and Link')
-        // also manipulate the Diagram by changing its Diagram.selection collection
-        var diagram = this.$refs.diag.diagram
-        diagram.select(diagram.findNodeForData(data))
-      },
+//    addNode: function () {
+//      var model = this.model()
+//      model.startTransaction()
+//      model.setDataProperty(model.findNodeDataForKey(4), 'color', 'purple')
+//      var data = {
+//        text: 'NEW ' + this.counter++,
+//        color: 'yellow'
+//      }
+//      model.addNodeData(data)
+//      model.addLinkData({
+//        from: 3,
+//        to: model.getKeyForNodeData(data)
+//      })
+//      model.commitTransaction('added Node and Link')
+//      // also manipulate the Diagram by changing its Diagram.selection collection
+//      var diagram = this.$refs.diag.diagram
+//      diagram.select(diagram.findNodeForData(data))
+//    },
 
       // Here we modify VUE's view model directly, and
       // then ask the GoJS Diagram to update everything from the data.
       // This is less efficient than calling the appropriate GoJS Model methods.
       // NOTE: Undo will not be able to restore all of the state properly!!
-      modifyStuff: function () {
-        var data = this.diagramData
-        data.nodeDataArray[0].color = 'red'
-        // Note here that because we do not have the GoJS Model,
-        // we cannot find out what values would be unique keys, for reference by the link data.
-        data.nodeDataArray.push({
-          key: ++this.counter2,
-          text: this.counter2.toString(),
-          color: 'orange'
-        })
-        data.linkDataArray.push({
-          from: 2,
-          to: this.counter2
-        })
-        this.updateDiagramFromData()
-      }
+//    modifyStuff: function () {
+//      var data = this.diagramData
+//      data.nodeDataArray[0].color = 'red'
+//      // Note here that because we do not have the GoJS Model,
+//      // we cannot find out what values would be unique keys, for reference by the link data.
+//      data.nodeDataArray.push({
+//        key: ++this.counter2,
+//        text: this.counter2.toString(),
+//        color: 'orange'
+//      })
+//      data.linkDataArray.push({
+//        from: 2,
+//        to: this.counter2
+//      })
+//      this.updateDiagramFromData()
+//    }
     }
   }
 </script>
--- src/views/sence/sence-modul.vue
+++ src/views/sence/sence-modul.vue
@@ -1,5 +1,13 @@
 <template>
-  <div style='border: solid 1px black;width:100%;height:400px'></div>
+  <div id="">
+    <span style="position: relative;display: inline-block; vertical-align: top; width:10%;height: 100%;">
+      <div id="myPaletteDiv" style="border: 1px solid #DBDBDB; height: 400px;"></div>
+    </span>
+    <span style="position: relative;display: inline-block; vertical-align: top; width:89%;height: 100%;">
+      <div id='myDiagram' style='border: solid 1px black;width:100%;height:400px'></div>
+      <div id="myOverviewDiv"></div> 
+    </span>
+  </div>
 </template>
 
 <script>
@@ -15,12 +23,24 @@
     mounted () {
       var $ = go.GraphObject.make
       var self = this
+      console.log(this.$el)
       var myDiagram =
-        $(go.Diagram, this.$el, {
+        $(go.Diagram, 'myDiagram', {
           initialContentAlignment: go.Spot.Center,
+          maxSelectionCount: 1, // users can select only one part at a time
+          validCycle: go.Diagram.CycleDestinationTree, // make sure users can only create trees
+          allowDrop: true, // from Palette
+          allowLink: false,
+          allowClipboard: true,
+          mouseDrop: function (e) {
+            e.diagram.currentTool.doCancel()
+//          finishDrop(e, true);
+          },
           layout: $(go.TreeLayout, {
+            treeStyle: go.TreeLayout.StyleLastParents,
             angle: 90,
-            arrangement: go.TreeLayout.ArrangementHorizontal
+            arrangement: go.TreeLayout.ArrangementHorizontal,
+            alternateAngle: 90
           }),
           'undoManager.isEnabled': true,
           // Model ChangedEvents get passed up to component users
@@ -32,38 +52,266 @@
           }
         })
 
+// 场景预览
+      var myOverview =
+        $(go.Overview, 'myOverviewDiv', {observed: myDiagram, contentAlignment: go.Spot.Center})
+
+      function textStyle () {
+        return {
+          font: '12pt  Segoe UI,sans-serif',
+          stroke: 'white'
+        }
+      }
+
       myDiagram.nodeTemplate =
-        $(go.Node, 'Auto',
-          $(go.Shape, {
-            fill: 'white',
-            strokeWidth: 0,
-            portId: '',
-            fromLinkable: true,
-            toLinkable: true,
-            cursor: 'pointer'
+        $(go.Node, 'Auto', {
+          doubleClick: function (e, node, prev) {
+
+          }
+        }, {
+          mouseDragEnter: function (e, node, prev) {
+
           },
-          new go.Binding('fill', 'color')),
-          $(go.TextBlock, {
-            margin: 8,
-            editable: true
+          mouseDragLeave: function (e, node, next) {
+
           },
-          new go.Binding('text').makeTwoWay())
+          mouseDrop: function (e, node) {
+
+          }
+        },
+        $(go.Shape, 'Rectangle', {
+          name: 'SHAPE',
+          fill: '#18D2D1',
+          stroke: null,
+          portId: '',
+          fromLinkable: false,
+          toLinkable: false,
+          cursor: 'pointer'
+        }),
+        $(go.Panel, 'Horizontal',
+          $(go.Panel, 'Table', {
+            margin: new go.Margin(0, 0, 0, 0),
+            defaultAlignment: go.Spot.Left
+          },
+          $(go.RowColumnDefinition, {
+            column: 5,
+            width: 15
+          }),
+          $(go.TextBlock, '条件：', textStyle(), {
+            row: 0,
+            column: 0,
+            margin: new go.Margin(8, 0, 8, 8)
+          }),
+          $(go.TextBlock, textStyle(),
+            {
+              wrap: go.TextBlock.WrapDesiredSize,
+              row: 0,
+              column: 1,
+              columnSpan: 5,
+              editable: false,
+              isMultiline: false,
+              margin: new go.Margin(8, 12, 8, 0),
+              maxSize: new go.Size(250, 999)
+            },
+            new go.Binding('text', 'description').makeTwoWay()),
+          $(go.TextBlock, '话术：', textStyle(), {
+            row: 1,
+            column: 0,
+            margin: new go.Margin(0, 0, 12, 8)
+          }),
+          $(go.TextBlock, textStyle(),
+            {
+              wrap: go.TextBlock.WrapDesiredSize,
+              row: 1,
+              column: 1,
+              columnSpan: 5,
+              editable: false,
+              isMultiline: false,
+              margin: new go.Margin(0, 12, 12, 0),
+              maxSize: new go.Size(250, 999)
+            },
+            new go.Binding('text', 'content').makeTwoWay())
+          )
+        ),
+        $('TreeExpanderButton', {
+          alignment: go.Spot.Bottom,
+          alignmentFocus: go.Spot.Top
+        }, {
+          visible: true
+        })
         )
 
       myDiagram.linkTemplate =
-        $(go.Link, {
+        $(go.Link, { selectable: false }, go.Link.Orthogonal, {
+          corner: 5,
           relinkableFrom: true,
           relinkableTo: true
         },
-          $(go.Shape),
-          $(go.Shape, {
-            toArrow: 'OpenTriangle'
-          })
+        $(go.Shape, {
+          strokeWidth: 4,
+          stroke: '#00a4a4'
+        })) // the link shape
+
+      function getNextKey () {
+        var key = nodeIdCounter
+        while (myDiagram.model.findNodeDataForKey(key) !== null) {
+          key = nodeIdCounter--
+        }
+        return key
+      }
+
+// 改变新老节点颜色
+      myDiagram.layout.commitNodes = function () {
+        myDiagram.layout.network.vertexes.each(function (v) {
+          if (v.node) {
+            console.log(v.node.data)
+            if (v.node.data.key > -1) {
+              var color = '#18D2D1'
+            } else {
+              var color = '#F0AD4E'
+            }
+            var shape = v.node.findObject('SHAPE')
+            if (shape) {
+              shape.fill = $(go.Brush, 'Linear', {
+                0: color,
+                1: go.Brush.lightenBy(color, 0.05),
+                start: go.Spot.Left,
+                end: go.Spot.Right
+              })
+            }
+          }
+        })
+      }
+
+      var nodeIdCounter = -1
+      myDiagram.nodeTemplate.contextMenu =
+        $(go.Adornment, 'Vertical',
+          $('ContextMenuButton',
+            $(go.TextBlock, '新增节点'), {
+              click: function (e, obj) {
+                var clicked = obj.part
+                if (clicked !== null) {
+                  var thisemp = clicked.data
+                  myDiagram.startTransaction('add employee')
+                  var newemp = {
+                    key: getNextKey(),
+                    content: '',
+                    description: '',
+                    parentId: thisemp.key
+                  }
+                  myDiagram.model.addNodeData(newemp)
+                  console.log(newemp)
+                  myDiagram.commitTransaction('add employee')
+                }
+              }
+            }
+          ),
+          $('ContextMenuButton',
+            $(go.TextBlock, '删除此节点'), {
+              click: function (e, obj) {
+
+              }
+            }
+          ),
+          $('ContextMenuButton',
+            $(go.TextBlock, '编辑此节点'), {
+              click: function (e, obj) {
+
+              }
+            }
+          )
         )
 
-      this.diagram = myDiagram
+// 左侧模板列表
+      var myPalette =
+        $(go.Palette, 'myPaletteDiv',  // must name or refer to the DIV HTML element
+          {
+            'animationManager.duration': 800 // slightly longer than default (600ms) animation
+          })
 
+      myPalette.nodeTemplateMap.add('', // the default category
+        $(go.Node, 'Spot',
+          $(go.Panel, 'Auto',
+            $(go.Shape, 'Rectangle',
+              { fill: '#18D2D1', stroke: null },
+              new go.Binding('figure', 'figure')),
+            $(go.TextBlock,
+              {
+                font: 'bold 11pt Helvetica, Arial, sans-serif',
+                stroke: '#ffffff',
+                margin: 8,
+                maxSize: new go.Size(120, NaN),
+                wrap: go.TextBlock.WrapFit,
+                editable: true
+              },
+              new go.Binding('text').makeTwoWay())
+          )
+      ))
+
+      var paletteData = [{
+        'id': 40,
+        'senceNo': null,
+        'senceTitle': '4.从技术到数据应用',
+        'useState': 0,
+        'startTime': null,
+        'endTime': null,
+        'createTime': '2018-06-26 15:55:08',
+        'creator': 'admin',
+        'senceClassify': null,
+        'submitTime': '2018-06-26 15:55:47',
+        'checkTime': null,
+        'submitter': 'admin',
+        'checker': null,
+        'classifys': [{
+          'id': 7,
+          'name': '奥法产品介绍',
+          'parentid': -1,
+          'level': null,
+          'type': null,
+          'group': 2
+        }]
+      }, {
+        'id': 39,
+        'senceNo': null,
+        'senceTitle': '3.智能巡检',
+        'useState': 0,
+        'startTime': null,
+        'endTime': null,
+        'createTime': '2018-06-26 15:53:22',
+        'creator': 'admin',
+        'senceClassify': null,
+        'submitTime': '2018-06-26 15:54:54',
+        'checkTime': null,
+        'submitter': 'admin',
+        'checker': null,
+        'classifys': [{
+          'id': 7,
+          'name': '奥法产品介绍',
+          'parentid': -1,
+          'level': null,
+          'type': null,
+          'group': 2
+        }]
+      }]
+      paletteData = JSON.stringify(paletteData)
+      paletteData = paletteData.replace(/id/g, 'key')
+      paletteData = paletteData.replace(/senceTitle/g, 'text')
+      paletteData = JSON.parse(paletteData)
+      myPalette.model = new go.GraphLinksModel(paletteData)
+// 模板列表完成
+      console.log(paletteData)
+      this.diagram = myDiagram
+      let updateModelData = JSON.stringify(this.modelData.nodeDataArray)
+//    updateModelData = updateModelData.replace(/parentId/g, 'parent')
+//    updateModelData = updateModelData.replace(/content/g, 'name')
+      updateModelData = updateModelData.replace(/id/g, 'key')
+      this.modelData.nodeDataArray = JSON.parse(updateModelData)
+//    myDiagram.model =
+//      $(go.TreeModel,
+//        { nodeParentKeyProperty: "parent",  // this property refers to the parent node data
+//          nodeDataArray: this.modelData.nodeDataArray });
       this.updateModel(this.modelData)
+//    console.log(this.modelData)
     },
     watch: {
       modelData: function (val) {
@@ -76,10 +324,12 @@
       },
       updateModel: function (val) {
         // No GoJS transaction permitted when replacing Diagram.model.
+        console.log(val)
         if (val instanceof go.Model) {
           this.diagram.model = val
         } else {
-          var m = new go.GraphLinksModel()
+          var m = new go.TreeModel()
+          m.nodeParentKeyProperty = 'parentId'
           if (val) {
             for (var p in val) {
               m[p] = val[p]
@@ -102,5 +352,14 @@
 </script>
 
 <style>
-
+#myOverviewDiv {
+  position: absolute;
+  width: 200px;
+  height: 100px;
+  top: 2px;
+  left: 2px;
+  background-color: aliceblue;
+  z-index: 300;
+  border: solid 1px blue;
+}
 </style>
